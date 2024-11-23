1b9e403c92de1b80c1e5eae31f09b93609ad3241
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-11-23 22:52:22 +0100

    Update version

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         in {
           default = pkgs.rustPlatform.buildRustPackage {
             name = "gen";
-            version = "0.0.1";
+            version = "1.0.0";
             cargoLock.lockFile = ./Cargo.lock;
             src = pkgs.lib.cleanSource ./.;
             meta = with pkgs.lib; {
