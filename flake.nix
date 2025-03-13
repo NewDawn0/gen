ad334cb57aae3eb361aa8730b0d802aa9879b603
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:09:53 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,43 +1,32 @@
 {
-  description = "An extensible project generator";
+  description = "Flexible tool for generating customizable project templates";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
-        };
-    in {
-      overlays.default =
-        (final: prev: { gen = self.packages.${prev.system}.default; });
-      packages = eachSystem (system:
-        let pkgs = mkPkgs system;
-        in {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "gen";
-            version = "1.0.0";
-            src = ./.;
-            cargoHash = "sha256-B+4jzreHXsAgE6noNzGajtKmJ1dLBN9E4q8ZRC9G6/0=";
-            meta = {
-              description =
-                "A flexible tool for generating customizable project templates";
-              longDescription = ''
-                This extensible project generator allows you to quickly set up new projects with customizable templates.
-                Ideal for users who want to automate the creation of project skeletons with personalized settings.
-              '';
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platforms = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      gen = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "gen";
+        version = "1.0.0";
+        src = ./.;
+        useFetchCargoVendor = true;
+        cargoHash = "sha256-QszcPmVDffCbbS1K9wm0BmVw8d8BxqoMFETmxJMfOdU=";
+        meta = {
+          description =
+            "Flexible tool for generating customizable project templates";
+          longDescription = ''
+            This extensible project generator allows you to quickly set up new projects with customizable templates.
+            Ideal for users who want to automate the creation of project skeletons with personalized settings.
+          '';
+          homepage = "https://github.com/NewDawn0/gen";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
